8530773b22e7d1e598f5294984709733cbb6cd2b
RustyRaptor <anonymous0programmer@gmail.com>
2018-06-29 13:26:28 -0600

    cleaned

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,3 @@
-// var Greeter = createReactClass({
-//         render: function() {
-//                 return (
-//                         <div>
-//                                 <h1>Hello React! e</h1>
-//                         </div>
-//                 )
-//         }
-// })
-
-
-
 class Greeter extends React.Component {
         render() {
           return <h1>React Test</h1>;
